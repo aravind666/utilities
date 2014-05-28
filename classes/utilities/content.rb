ecfe1c921248a1e7219b6e0ef0018347dafa4e3e
aravind666 <aravind.udayashankara@gmail.com>
2014-05-28 16:50:51 +0530

    Refactored content and message classes

---
+++ classes/utilities/content.rb
@@ -0,0 +1,122 @@
+# content class which defines various attributes and behaviours which are used in
+# migration
+#
+# Author::    Aravind Udayashankara  (mailto:aravind.udayashankara@gmail.com)
+# Copyright:: Copyright (c) 2012 Crossroads
+# License::   MIT
+#
+#
+class Content
+
+  #
+  # Create Content object by initilizing the migration flow
+  #
+  def initialize
+    self.clean_old_files();
+    self.migrate_content();
+  end
+
+  #
+  # This method clears old files by deleting the destination directory to recreate it
+  #
+  def clean_old_files
+    begin
+      Contenthelper.validate_content_destination_path;
+      FileUtils.rm_rf(Dir.glob(Immutable.config.content_destination_path))
+    rescue Errno::ENOENT => e
+      Immutable.log.error "Folder does not exists, Its first run #{e}"
+    end
+  end
+
+  #
+  # Migration bootstrap
+  #
+  def migrate_content
+    content_data = Contenthelper.get_content_from_database();
+    self.process_content(content_data);
+  end
+
+  #
+  # This method is used to process fetched content
+  #
+  def process_content(content)
+    begin
+      while row = content.fetch do
+        self.build_content_based_on_status(row);
+      end
+      Immutable.log.info "Completed Migration "
+      abort("\n Migration process successfully completed, Please check migration.log \n");
+    end
+  end
+
+  #
+  # This method is used to setup folders for category
+  # and also the content directory,
+  # It deletes the content directory if it exists and re creates it
+  #
+  def setup_file_path(category_name)
+
+    destination_dir = Immutable.config.content_destination_path
+    unless File.directory?(destination_dir)
+      FileUtils.mkdir_p(destination_dir)
+    end
+
+    dirname = Immutable.config.content_destination_path + '/' + category_name.gsub(/\s/,'-');
+    unless File.directory?(dirname)
+      FileUtils.mkdir_p(dirname)
+    end
+
+  end
+
+  #
+  # This method initiates migration based on the
+  # status of the content which is about to get migrated
+  #
+  def build_content_based_on_status(content)
+    db_file_path = Contenthelper.purify_file_path(content[1]);
+    destination_file_name = content[2];
+    complete_source_path = Immutable.config.content_source_path + db_file_path + destination_file_name;
+    category_name = content[3].gsub(/\s/,'-');
+    status = File.file?(complete_source_path);
+    case status
+      when true
+        self.setup_file_path(category_name);
+        front_matter = get_jekyll_front_matter_for_content(content);
+        self.migrate_by_adding_jekyll_front_matter(complete_source_path, destination_file_name, category_name, front_matter);
+      when false
+        Immutable.log.warn " - Source WebPage ID #{content['web_page_id']} does not exists at #{complete_source_path} "
+    end
+  end
+
+  #
+  # This method actually migrates the content by creating the front matter
+  #
+  def migrate_by_adding_jekyll_front_matter(complete_source_path, file_name, category_name,front_matter)
+
+    source_file_handler = File.open(complete_source_path)
+    data_to_migrate = source_file_handler.read();
+    migrated_file_path = "#{Immutable.config.content_destination_path}/#{category_name}/#{file_name}";
+    migrated_content_file_handler = File.open(migrated_file_path, 'w');
+    migrated_content_file_handler.write(front_matter);
+
+    # TODO : - Research not sure why this wont work in Mac system
+    # data_to_migrate.delete!("\C-M");
+    migrated_content_file_handler.write(data_to_migrate);
+  end
+
+  #
+  # This method is used to get jekyll front matter for a particular content
+  #
+  def get_jekyll_front_matter_for_content(content)
+
+    file_path = content[1];
+    title = content[0].gsub(':','-');
+    category = content[3].to_s;
+    if file_path['shortlink']
+      return "---\nlayout: #{content[4]}\ntitle: #{title}\ncategory: #{category}\npermalink: /#{content[2].gsub('.htm','');}/\n---\n"
+    else
+      return "---\nlayout: #{content[4]}\ntitle: #{title}\ncategory: #{category}\n---\n";
+    end
+  end
+
+end
---
+++ classes/utilities/message.rb
@@ -0,0 +1,147 @@
+# encoding: ASCII-8BIT
+
+# message class which defines various attributes and behaviours which are used in
+# message migration
+#
+# Author::    Aravind Udayashankara  (mailto:aravind.udayashankara@gmail.com)
+# Copyright:: Copyright (c) 2012 Crossroads
+# License::   MIT
+#
+#
+class Message
+
+  #
+  # Create Message object by initilizing the migration flow
+  #
+  def initialize
+    self.migrate_messages();
+  end
+
+  #
+  # This method actually migrates the content by creating the front matter
+  #
+  def migrate_messages
+    begin
+      series_data = Mediahelper.get_all_series();
+      self.process_series_data(series_data);
+    end
+  end
+
+  #
+  # This method will process series data by collecting all its respective messages
+  #
+  def process_series_data(series_data)
+    begin
+      series_data.each do |series|
+        message_data = Mediahelper.get_all_messages_for_series(series[0]);
+        process_message_data(message_data, series);
+      end
+      abort('Successfully migrated messages in specified destination');
+    end
+  end
+
+  #
+  # This method will process message details by collecting
+  # all its media content before migration
+  #
+  def process_message_data(message_data, series)
+    begin
+      message_data.each do |message|
+        media_content = Mediahelper.get_media_content_for_message(message[0]);
+        if media_content.column_names.size === 0 then
+          Immutable.log.info "Message  #{message[0]} does not have any media content";
+        else
+          front_matter = self.get_jekyll_frontmatter_for_messages(message, series, media_content);
+          self.migrate_by_adding_jekyll_front_matter(front_matter, message);
+        end
+      end
+    end
+  end
+
+  #
+  # Prepare Jekyll Frontmatter for migrated messages
+  #
+  #
+  def get_jekyll_frontmatter_for_messages(message_data, series, media_content)
+    begin
+      front_matter = '';
+      mainTitle = message_data[2].gsub /"/, '';
+      front_matter = "---\nlayout: message\ncategory: message\nseries: \"#{series[1]}\"\ntitle: \"#{mainTitle}\"";
+      front_matter += "\ndate: #{message_data["Date"].strftime("%Y-%m-%d")}"
+      front_matter = self.add_media_content_front_matter(media_content,front_matter);
+      front_matter += "\n---";
+      return front_matter
+    end
+  end
+
+  #
+  # Add media content front matter which is consumed by
+  # liquid variables in message layout .
+  #
+  def add_media_content_front_matter(media_content, front_matter)
+    begin
+      media_content.each do |media|
+        #
+        # A message can have multiple media contents we need to look for
+        # all possiblilities also each media has its own description and title we need them too ..
+        #
+        case media['ContentTypeID']
+          when 5
+            # Audio
+            audio_description = Contenthelper.purify_by_removing_special_characters(media['Description']);
+            audio = media['LowQFilePath'] + media['HighQFilePath'];
+            audio_title = Contenthelper.purify_by_removing_special_characters(media['Title']);
+            front_matter += "\naudio-description: \"#{audio_description}\"\naudio: \"#{audio}\"\naudio-title: \"#{audio_title}\""
+            front_matter += "\naudio-duration: \"#{media['duration']}\"";
+          when 4
+            # Video -- only IPOD video
+            if (media['iPodVideo'].length > 0)
+              video_description = Contenthelper.purify_by_removing_special_characters(media['Description']);
+              video_title = Contenthelper.purify_by_removing_special_characters(media['Title']);
+              video_poster = media['ThumbImagePath'];
+              front_matter += "\nvideo-description: \"#{video_description}\"\nvideo-title: \"#{video_title}\""
+              front_matter += "\nvideo: \"#{media['iPodVideo']}\"";
+              front_matter += "\nvideo-poster: \"#{Immutable.config.image_thumb_base_url}#{video_poster}\"";
+            end
+          when 7
+            # Study Notes
+            notes_description = Contenthelper.purify_by_removing_special_characters(media['Description']);
+            notes = media['LowQFilePath'] + media['HighQFilePath'];
+            notes_title = Contenthelper.purify_by_removing_special_characters(media['Title']);
+            front_matter += "\nnotes-description: \"#{notes_description} \"\nnotes: \"#{notes} \"\nnotes-title: \"#{notes_title}\""
+          when 8
+            # Weekend Program
+            if(media['Description'].length > 0)
+              program_description = Contenthelper.purify_by_removing_special_characters(media['Description']);
+            else
+              program_description = '';
+            end
+            program = media['LowQFilePath'] + media['HighQFilePath'];
+            program_title = media['Title'];
+            front_matter += "\nprogram-description: \"#{program_description}\"\nprogram: \"#{program}\"\nprogram-title: \"#{program_title}\""
+        end
+      end
+      return front_matter;
+    end
+  end
+
+
+  #
+  # Creates a jekyll page by applying neccessary frontmatter
+  #
+  def migrate_by_adding_jekyll_front_matter(jekyll_front_matter, message_data)
+    begin
+      target_file_path = "#{Immutable.config.message_destination_path}/";
+      target_file_path += "#{message_data["Title"].downcase.gsub(' ', '_').gsub('/', '-').gsub('?','').gsub('*','').gsub('#','').gsub('@','').gsub('&','_and_')}.md"
+      # lets remove only quotes in the file name since its non standard
+      target_file_path = target_file_path.gsub /"/, '';
+      target_file_path = target_file_path.gsub '|', '';
+      target_file_path = target_file_path.gsub ':', '';
+
+      migrated_message_file_handler = File.open(target_file_path, 'w');
+      migrated_message_file_handler.write(jekyll_front_matter);
+    end
+  end
+
+
+end
