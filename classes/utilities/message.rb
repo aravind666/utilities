7c03178b4785f12a0cebb418455e8714931410e3
aravind666 <aravind.udayashankara@gmail.com>
2014-05-29 12:59:11 +0530

    Fixed same title and file name issue identified by chris

--- classes/utilities/message.rb
+++ classes/utilities/message.rb
@@ -142,6 +142,9 @@ class Message
       target_file_path = target_file_path.gsub ':', '';
       target_file_path = target_file_path.gsub '__', '_';
       target_file_path = target_file_path.gsub '_-_', '_';
+      target_file_path = target_file_path.gsub ',_', '_';
+      target_file_path = target_file_path.gsub '_<i>', '_';
+
       migrated_message_file_handler = File.open(target_file_path, 'w');
       migrated_message_file_handler.write(jekyll_front_matter);
     end
