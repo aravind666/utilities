f508b506c6842827ab7f31abf0e693f49a87e1e0
hanumanthraju <hanumanthraju.t@gmail.com>
2014-07-01 19:17:38 +0530

    MIL-314 youtube utilit module added

---
+++ classes/helpers/youtubemodule.rb
@@ -0,0 +1,7 @@
+module YouTubeModule
+  YOUTUBE_READONLY_SCOPE = 'https://www.googleapis.com/auth/youtube.readonly'
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
+  USER_ACCESS_TOKEN_INFO = Immutable.config.user_oauth_json
+  YOUTUBE_CLIENT_SECRETE = Immutable.config.youtube_client_secrete_json
+end
