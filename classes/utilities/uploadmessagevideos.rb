0c18abbcc5902ac31e27f998f28dfacde9e10f28
hanumanthraju <hanumanthraju.t@gmail.com>
2014-07-02 17:55:07 +0530

    MIL-314 upload message videosto youtube script added

---
+++ classes/utilities/uploadmessagevideos.rb
@@ -0,0 +1,38 @@
+# encoding: ASCII-8BIT
+#
+# UploadVideos. class which defines various attributes and behaviours
+# which are used to upload videos to youtube
+#
+# Author::    Hanumantharaju  (mailto:hanumantharaju.tswamy@costrategix.com)
+# Copyright:: Copyright (c) 2012 Crossroads
+# License::   MIT
+#
+# Initiating this class leads to uploading videos to youtube
+#
+class UploadMessageVideos
+
+  #
+  # Initialize the video uploading videos process
+  #
+  def initialize
+    self.upload_message_video
+  end
+
+  #
+  # Function used to upload message videos to youtube
+  #
+  def upload_message_video
+    begin
+      youtube_response = Hash.new
+      youtube_response['youtube_video_id'] = 0
+      get_all_video_data = YouTubeHelper.get_message_video_data
+      get_all_video_data.each do |video_data|
+        youtube_response = YouTubeHelper.upload_video_to_youtube(video_data)
+        puts "Video file '#{video_data[:file]}' uploaded successfully"
+        video_id = YouTubeHelper.normalize_response_data(youtube_response)
+        YouTubeHelper.create_entry_in_db(video_id, video_data)
+      end
+    end
+  end
+
+end
